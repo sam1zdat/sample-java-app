5224ae5a635e7ccaa77654d955fd848065b6b6fe
samizdat <alfred.delay@gmail.com>
2025-05-23 00:36:58 +0200

    Create DemoApplication.java

---
+++ DemoApplication.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class DemoApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello from custom S2I Java builder!");
+        while (true) {}
+    }
+}
